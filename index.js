3b68423139aa384dfb1dfd1bf566b31410616fc5
Jonathan Luk <jluk87@gmail.com>
2019-08-14 22:37:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ const getFirstSelector = function (selector) {
    const everyDiv = document.querySelectorAll('#grand-node div');
    return everyDiv[everyDiv.length-1];
  }
+
+const increaseRankBy = function (n) {
+  const allRanks = document.querySelectorAll('.ranked-list li')
+}
