f9dc598f28f8d7ea02cf5f909292a1f516081a1b
Jonathan Luk <jluk87@gmail.com>
2019-08-14 22:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const getFirstSelector = function (selector) {
     return document.querySelector(selector);
 }
+ const nestedTarget = function () {
+   const nested = document.getElementById('nested');
+   return nested.getElementsByClassName('target');
+ }
